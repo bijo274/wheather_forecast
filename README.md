9cf6f2226cbd6ce1af0cbc41ae2a3976d07a5c80
Bijoy <bbiswas@mscripts.com>
2018-04-22 21:34:19 +0530

    project upload and configs files

---
+++ README.md
@@ -0,0 +1,8 @@
+# wheather_forecast
+
+A wheather forecast project
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Wheather',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Wheather'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  String name;
+  _MyHomePageState.named();
+}
