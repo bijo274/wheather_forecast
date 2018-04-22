e665ff99e3d6b9fe93c258b009d9b0b8a6032495
Bijoy <bbiswas@mscripts.com>
2018-04-22 23:56:04 +0530

    impl

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new Wheather());
 
-class MyApp extends StatelessWidget {
+class Wheather extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -18,12 +19,31 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage>{
-  String name;
-  _MyHomePageState.named();
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Stack(
+        children: <Widget>[
+          new Image.asset(
+            'assets/wheather_bg.jpg',
+            fit: BoxFit.cover,
+          ),
+          new ClipOval(
+            child: new Image.asset(
+              'assets/wheather_bg.jpg',
+              fit: BoxFit.cover,
+            ),
+          )
+
+        ],
+      ),
+    );
+    // TODO: implement build
+  }
+
 }
